baf6660eec5871053870ebe10f0462896bb7ae47
David Ferrand <d.ferrand@emogi.com>
2020-10-10 21:06:36 +0200

    Simple version that reads list of recipes from Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:projectquiche/screens/authenticate.dart';
-import 'package:projectquiche/screens/recipe_list.dart';
+import 'package:projectquiche/screens/cloud_recipe_list.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 
@@ -10,25 +10,33 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(new MaterialApp(
-    theme: ThemeData.dark(),
-    home: await getLandingPage(),
-    // initialRoute: '/',
-    // routes: <String, WidgetBuilder>{
-    //   '/': (BuildContext context) => AuthenticatePage(),
-    // },
-  ));
+  runApp(QuicheApp());
 }
 
-Future<Widget> getLandingPage() async {
-  return StreamBuilder<User>(
-    stream: _auth.authStateChanges(),
-    builder: (BuildContext context, snapshot) {
-      if (snapshot.hasData && (!snapshot.data.isAnonymous)) {
-        return RecipeListPage();
-      }
+class QuicheApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      theme: ThemeData.dark(),
+      home: getLandingPage(),
+      // initialRoute: '/',
+      // routes: <String, WidgetBuilder>{
+      //   '/': (BuildContext context) => AuthenticatePage(),
+      // },
+    );
+  }
 
-      return AuthenticatePage();
-    },
-  );
+  Widget getLandingPage() {
+    return StreamBuilder<User>(
+      stream: _auth.authStateChanges(),
+      builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+        if (snapshot.hasData && !snapshot.data.isAnonymous) {
+          return CloudRecipeListPage();
+          // return RecipeListPage();
+        }
+
+        return AuthenticatePage();
+      },
+    );
+  }
 }
