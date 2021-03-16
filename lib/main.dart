33870bb50b1f3d82f67d8d2864fd8d0e4abb977b
David Ferrand <d.ferrand@emogi.com>
2021-03-16 20:10:22 +0100

    Add Crashlytics. Pass crashes and non-fatal errors.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:projectquiche/pages/authenticate.dart';
 import 'package:projectquiche/pages/main_app_scaffold.dart';
@@ -11,6 +12,9 @@ final FirebaseAuth _auth = FirebaseAuth.instance;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  // Pass Flutter errors to Crashlytics. This still prints to the console too.
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
 
   runApp(QuicheApp());
 }
@@ -35,6 +39,8 @@ class QuicheApp extends StatelessWidget {
         log("Logged in user ID: ${snapshot.data?.uid}, email: ${snapshot.data?.email}");
 
         if (snapshot.hasData && snapshot.data?.isAnonymous != true) {
+          FirebaseCrashlytics.instance
+              .setUserIdentifier(snapshot.data?.uid ?? "unknown");
           return MainAppScaffold();
         }
 
