05c04110d18cc5b3c33938dd9b2ad21356e529f0
David Ferrand <d.ferrand@emogi.com>
2021-03-16 23:46:43 +0100

    New recipe screen had richer text editing with tabs

--- lib/pages/new_recipe.dart
+++ lib/pages/new_recipe.dart
@@ -11,53 +11,80 @@ class NewRecipePage extends StatefulWidget {
 }
 
 class _NewRecipePageState extends State<NewRecipePage> {
-  var _recipeName = "";
-  var _recipeIngredients = "";
-  var _recipeSteps = "";
-  var _recipeTips = "";
+  String _recipeName = "Untitled";
+  String? _recipeIngredients;
+  String? _recipeSteps;
+  String? _recipeTips;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text("New recipe")),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("New recipe"),
+          actions: [
+            IconButton(icon: Icon(Icons.save), onPressed: _postNewRecipe)
+          ],
+          bottom: TabBar(
+            tabs: [
+              Tab(text: "Info"),
+              Tab(text: "Ingredients"),
+              Tab(text: "Steps"),
+              Tab(text: "Tips"),
+            ],
+          ),
+        ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: ListView(
+          child: TabBarView(
             children: [
               TextFormField(
-                decoration: const InputDecoration(labelText: "Name"),
+                decoration: InputDecoration(labelText: "Name"),
+                textCapitalization: TextCapitalization.sentences,
                 onChanged: (text) {
                   _recipeName = text;
                 },
               ),
               TextFormField(
-                decoration: const InputDecoration(labelText: "Ingredients"),
-                keyboardType: TextInputType.multiline,
+                // Ingredients
+                decoration:
+                    InputDecoration(hintText: "- 20g butter\n- 4 eggs\n..."),
+                textCapitalization: TextCapitalization.sentences,
                 onChanged: (text) {
                   _recipeIngredients = text;
                 },
+                expands: true,
+                maxLines: null,
+                minLines: null,
               ),
               TextFormField(
-                decoration: const InputDecoration(labelText: "Steps"),
-                keyboardType: TextInputType.multiline,
+                decoration: InputDecoration(
+                    hintText: "1. Preheat oven to 220°C\n2. Cut stuff\n..."),
+                textCapitalization: TextCapitalization.sentences,
                 onChanged: (text) {
                   _recipeSteps = text;
                 },
+                expands: true,
+                maxLines: null,
+                minLines: null,
               ),
               TextFormField(
-                decoration: const InputDecoration(labelText: "Tips"),
-                keyboardType: TextInputType.multiline,
+                decoration: InputDecoration(
+                    hintText: "Serve with mixed greens or mash potato.\n..."),
+                textCapitalization: TextCapitalization.sentences,
                 onChanged: (text) {
                   _recipeTips = text;
                 },
+                expands: true,
+                maxLines: null,
+                minLines: null,
               ),
-              ElevatedButton(
-                child: Text('Add'),
-                onPressed: _postNewRecipe,
-              )
             ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 
   void _postNewRecipe() {
